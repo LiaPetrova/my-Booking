29c881e650d2393c4bb6118aaa3aa755a3c6b3c9
LiaPetrova <96750474+LiaPetrova@users.noreply.github.com>
2022-09-22 23:33:40 +0200

    Added CSS; Implemented Catalog and Details;

--- index.js
+++ index.js
@@ -3,13 +3,28 @@ const hbs = require('express-handlebars').create({
     extname: '.hbs'
 });
 
+const defaultController = require('./controllers/defaultController');
+const homeController = require('./controllers/homeController');
+const catalogController = require('./controllers/catalogController');
+const createController = require('./controllers/createController');
+const defaultTitle = require('./middlewares/defaultTitle');
+
+
 
 const app = express();
 
 app.engine('.hbs', hbs.engine);
 app.set('view engine', '.hbs');
 
+app.use(defaultTitle('Softuni Accomodation'));
+
 app.use(express.urlencoded({extended: true}));
 app.use('/static', express.static('static'));
+
+app.use(homeController);
+//attach all controllers
+app.use('/catalog', catalogController);
+app.use('/create', createController);
+app.all('*', defaultController);
 
 app.listen(3000, () => console.log('Server listening on Port 3000'));
