0e44c784bb668d2abffba61694149279e3a8cda9
LiaPetrova <96750474+LiaPetrova@users.noreply.github.com>
2022-09-30 00:48:46 +0200

    Add Faciliy model and controller

--- config/routes.js
+++ config/routes.js
@@ -2,11 +2,13 @@ const defaultController = require('../controllers/defaultController');
 const homeController = require('../controllers/homeController');
 const catalogController = require('../controllers/catalogController');
 const createController = require('../controllers/createController');
+const facilityController = require('../controllers/facilityController');
 
 module.exports = (app) => {
     app.use(homeController);
 //attach all controllers
 app.use('/catalog', catalogController);
 app.use('/create', createController);
+app.use('/facility', facilityController);   
 app.all('*', defaultController);
 };  
