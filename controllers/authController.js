da9e7d6193fac9c4fe92b53f14b109a8b5542bb8
LiaPetrova <96750474+LiaPetrova@users.noreply.github.com>
2022-10-07 13:33:32 +0200

    Authentication controller added

---
+++ controllers/authController.js
@@ -0,0 +1,11 @@
+const authController = require('express').Router();
+
+authController.get('/obtain', (req, res) => {
+
+});
+
+authController.get('/validate', (req, res) => {
+
+});
+
+module.exports = authController;
