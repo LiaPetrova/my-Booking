f9d9314fde098697d8fc9d14111aea70bcebfe1c
LiaPetrova <96750474+LiaPetrova@users.noreply.github.com>
2022-10-11 02:28:00 +0200

    Add validator libs; add error parser

---
+++ utils/parser.js
@@ -0,0 +1,27 @@
+function parseError (error) {
+
+    const result = {
+        messages: [],
+        fields: {}
+    };
+
+    if(error.name == 'ValidationError') {
+        //mongoose
+        for(let [field, e] of Object.entries(error.errors)) {
+            result.fields[field] = field;
+            result.messages.push(e.message);
+        }
+    } else if (Array.isArray(error)) {
+        // express-validator
+        result.fields = Object.fromEntries(error.map(e => [e.param, e.param]));
+        result.messages = error.map(e => e.msg);
+    } else {
+        result.messages.push(error.message);
+    }
+
+    return result;
+}
+
+module.exports = {
+    parseError
+}; 
